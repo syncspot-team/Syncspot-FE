92c4dff9938431361965bbdbf389bc911a44e855
TaeYoon Kim <xodbsrla1025@naver.com>
2024-12-17 14:15:50 +0900

    [Chore] CI/CD 구축 (#11)
    * chore: cicd 구축
    
    * docs: 마이그레이션 테스트용 문구 변경

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   };
   return (
     <>
-      <h1>Home</h1>
+      <h1>Home 마이그레이션 version 4.5.1</h1>
       <button onClick={handleClick}>click me</button>
     </>
   );
