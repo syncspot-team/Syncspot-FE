1b79e7f9de3251ec74be47b2e6d51aa762e9d499
Cllaude99 <xodbsrla1025@naver.com>
2024-12-18 13:35:03 +0900

    feat: 공통 버튼 컴포넌트 구현

---
+++ src/components/common/Buttons/Button.tsx
@@ -0,0 +1,65 @@
+import { cva, VariantProps } from 'class-variance-authority';
+import { mergeClassNames } from '@src/utils/mergeClassNames';
+import { ButtonHTMLAttributes } from 'react';
+
+export const ButtonVariants = cva(
+  `
+  flex items-center justify-center   
+  disabled:cursor-not-allowed
+  disabled:bg-disabled
+  disabled:border-disabled
+  `,
+  {
+    variants: {
+      buttonType: {
+        primary:
+          'w-max h-[4rem] bg-primary rounded-default border-primary py-[1.125rem] px-[12.3125rem] hover:bg-secondary hover:border-secondary text-white-default',
+      },
+      fontSize: {
+        default: 'text-menu',
+      },
+    },
+    defaultVariants: {
+      buttonType: 'primary',
+      fontSize: 'default',
+    },
+  },
+);
+
+interface IButton {
+  isLoading?: boolean;
+  disabled?: boolean;
+}
+
+interface IButtonProps
+  extends VariantProps<typeof ButtonVariants>,
+    ButtonHTMLAttributes<HTMLButtonElement>,
+    IButton {
+  className?: string;
+  onClick: () => void;
+  children: React.ReactNode;
+}
+
+function Button({
+  buttonType,
+  fontSize,
+  className,
+  children,
+  onClick,
+  isLoading,
+  disabled,
+}: IButtonProps) {
+  return (
+    <button
+      className={mergeClassNames(
+        ButtonVariants({ buttonType, fontSize, className }),
+      )}
+      onClick={onClick}
+      disabled={disabled || isLoading}
+    >
+      {children}
+    </button>
+  );
+}
+
+export default Button;
