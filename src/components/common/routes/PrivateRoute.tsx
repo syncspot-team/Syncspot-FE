92476ad6299fa5beed6d6828e49dda5656c48b4d
Cllaude99 <xodbsrla1025@naver.com>
2024-12-20 13:18:23 +0900

    feat: private, public route 설정

--- src/components/common/routes/PrivateRoute.tsx
+++ src/components/common/routes/PrivateRoute.tsx
@@ -1,14 +1,13 @@
 import { PATH } from '@src/constants/path';
 import { loginAtom } from '@src/state/store/login';
-import { Navigate } from 'react-router-dom';
+import { Navigate, Outlet } from 'react-router-dom';
 import { useAtomValue } from 'jotai';
-import { Outlet } from 'react-router-dom';
 
-export const PrivateRoute = () => {
+export default function PrivateRoute() {
   const isLogin = useAtomValue(loginAtom);
 
   if (!isLogin) {
     return <Navigate to={PATH.LOGIN} replace />;
   }
   return <Outlet />;
-};
+}
