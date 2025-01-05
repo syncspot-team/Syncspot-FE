17578d4080d47c164191367195b9fe9b6ed9b2ac
Cllaude99 <xodbsrla1025@naver.com>
2025-01-05 15:15:17 +0900

    feat: 회원이 속한 방을 조회하는 비동기 요청

---
+++ src/apis/header/getJoinRoom.ts
@@ -0,0 +1,10 @@
+import { API } from '@src/constants/api';
+import { IJoinRoomResponse } from '@src/types/header/joinRoomResponseType';
+import getAPIResponseData from '@src/utils/getAPIResponseData';
+
+export const getJoinRoom = () => {
+  return getAPIResponseData<IJoinRoomResponse>({
+    method: 'GET',
+    url: API.JOINED_ROOMS_SEARCH,
+  });
+};
