0f21196d7c840774cc85c5d758fa057c9d92d4a3
Cllaude99 <xodbsrla1025@naver.com>
2025-01-06 18:38:33 +0900

    fix: 로그인 버튼 네이밍 변경사항 반영

--- src/pages/auth/SignInPage.tsx
+++ src/pages/auth/SignInPage.tsx
@@ -3,12 +3,12 @@ import IconOauthKakao from '@src/assets/icons/IconOauthKakao.svg?react';
 import IconOauthNaver from '@src/assets/icons/IconOauthNaver.svg?react';
 import IconOauthGoogle from '@src/assets/icons/IconOauthGoogle.svg?react';
 import { useForm } from 'react-hook-form';
-import LoginButton from '@src/components/common/buttons/SignInButton/SignInButton';
 import { useState } from 'react';
 import { ISignInRequest } from '@src/types/auth/SignInRequestType';
 import { useNavigate } from 'react-router-dom';
 import { PATH } from '@src/constants/path';
 import { useSignInMutation } from '@src/state/mutations/auth/useSignInMutation';
+import SignInButton from '@src/components/common/buttons/SignInButton/SignInButton';
 
 export default function SignInPage() {
   const navigate = useNavigate();
@@ -51,7 +51,7 @@ export default function SignInPage() {
           placeholder="비밀번호 (영문 대/소문자, 숫자, 특수문자 포함)"
           className="placeholder:text-gray-normal bg-gray-light  py-[1.3125rem] px-[1.5rem] rounded-default mb-5"
         />
-        <LoginButton isLoading={formLoading} disabled={!isFormValid} />
+        <SignInButton isLoading={formLoading} disabled={!isFormValid} />
         <span className="flex justify-end text-gray-normal mt-[0.875rem] mb-[2.5rem]">
           아이디/비밀번호 찾기
         </span>
