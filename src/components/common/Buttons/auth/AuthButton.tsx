b8003f9f44d5e98169cfd906a4060ef9a98e05da
Cllaude99 <xodbsrla1025@naver.com>
2025-01-06 19:19:42 +0900

    fix: Auth버튼 경로 문제 해결

---
+++ src/components/common/Buttons/auth/AuthButton.tsx
@@ -0,0 +1,25 @@
+import Button from '../Button';
+
+interface IAuthButton {
+  buttonText: string;
+  isLoading: boolean;
+  disabled: boolean;
+}
+
+export default function AuthButton({
+  buttonText,
+  isLoading,
+  disabled,
+}: IAuthButton) {
+  return (
+    <Button
+      type="submit"
+      buttonType="primary"
+      fontSize="default"
+      isLoading={isLoading}
+      disabled={disabled}
+    >
+      {buttonText}
+    </Button>
+  );
+}
