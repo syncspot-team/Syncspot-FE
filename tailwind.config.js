d422181cf11f55b44b8d77f6ce05409408d9bc84
Cllaude99 <xodbsrla1025@naver.com>
2025-01-06 15:45:00 +0900

    chore: fontSize에 logo 사이즈 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,9 +8,11 @@ export default {
         pre: 'Pretendard',
       },
       fontSize: {
-        // 제목 - SUIT Bold 38pt
+        // 로고 - SUIT Bold 38pt
+        logo: ['2.375rem', { letterSpacing: '-2.5%', fontWeight: '700' }],
+        // 제목 - SUIT Bold 26pt
         title: [
-          '2.375rem',
+          '1.625rem',
           {
             letterSpacing: '-2.5%',
             fontWeight: '700',
