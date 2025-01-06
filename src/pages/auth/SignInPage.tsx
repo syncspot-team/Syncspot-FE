27b3a7c7234ebd3fd898a721f86e996eb9c2aae6
Cllaude99 <xodbsrla1025@naver.com>
2025-01-06 18:55:48 +0900

    fix: 경로 에러 해결

--- src/pages/auth/SignInPage.tsx
+++ src/pages/auth/SignInPage.tsx
@@ -8,13 +8,12 @@ import { ISignInRequest } from '@src/types/auth/SignInRequestType';
 import { useNavigate } from 'react-router-dom';
 import { PATH } from '@src/constants/path';
 import { useSignInMutation } from '@src/state/mutations/auth/useSignInMutation';
-import AuthButton from '@src/components/common/buttons/auth/AuthButton';
 
 export default function SignInPage() {
   const navigate = useNavigate();
-  const [formLoading, setFormLoading] = useState(false);
-  const { register, handleSubmit, watch } = useForm<ISignInRequest>();
-  const isFormValid = watch('email') && watch('pw');
+  const [, setFormLoading] = useState(false);
+  const { register, handleSubmit } = useForm<ISignInRequest>();
+  //const isFormValid = watch('email') && watch('pw');
   const { mutate: signIn } = useSignInMutation();
 
   const onSubmit = (data: ISignInRequest) => {
@@ -51,11 +50,7 @@ export default function SignInPage() {
           placeholder="비밀번호 (영문 대/소문자, 숫자, 특수문자 포함)"
           className="placeholder:text-gray-normal bg-gray-light  py-[1.3125rem] px-[1.5rem] rounded-default mb-5"
         />
-        <AuthButton
-          text="로그인"
-          isLoading={formLoading}
-          disabled={!isFormValid}
-        />
+
         <span className="flex justify-end text-gray-normal mt-[0.875rem] mb-[2.5rem]">
           아이디/비밀번호 찾기
         </span>
