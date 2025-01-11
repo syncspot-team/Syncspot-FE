03784b0af6fb305c0609245e687607e9db200efe
Cllaude99 <xodbsrla1025@naver.com>
2025-01-11 20:48:29 +0900

    feat: 디바운스 훅 구현

---
+++ src/hooks/useDebounce.ts
@@ -0,0 +1,16 @@
+import { useEffect } from 'react';
+import { useState } from 'react';
+
+export function useDebounce(value: string, delay: number) {
+  const [debouncedValue, setDebouncedValue] = useState(value);
+
+  useEffect(() => {
+    const timerId = setTimeout(() => {
+      setDebouncedValue(value);
+    }, delay);
+
+    return () => clearTimeout(timerId);
+  }, [value, delay]);
+
+  return debouncedValue;
+}
