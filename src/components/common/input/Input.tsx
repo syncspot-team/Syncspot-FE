9a4640b4e3c7aea8bc5f92986993e9361f7edc8a
Cllaude99 <xodbsrla1025@naver.com>
2025-01-14 19:34:30 +0900

    feat: 공통 Input 컴포넌트 개선

--- src/components/common/input/Input.tsx
+++ src/components/common/input/Input.tsx
@@ -1,25 +1,15 @@
-import React from 'react';
+import React, { forwardRef } from 'react';
 
-interface IInput {
-  placeholder: string;
-  type: 'text' | 'email' | 'password' | string;
-  maxLength: number;
-  [key: string]: any;
-}
+type InputProps = React.InputHTMLAttributes<HTMLInputElement>;
 
-const Input = React.forwardRef<HTMLInputElement, IInput>(
-  ({ placeholder, type, maxLength, ...props }, ref) => {
-    return (
-      <input
-        ref={ref}
-        className="w-full py-2 pl-5 transition border-none outline-none rounded-xl h-[4rem] bg-gray-light focus:shadow-focus placeholder:text-gray-normal"
-        type={type}
-        placeholder={placeholder}
-        maxLength={maxLength}
-        {...props}
-      />
-    );
-  },
-);
+export const Input = forwardRef<HTMLInputElement, InputProps>((props, ref) => {
+  return (
+    <input
+      ref={ref}
+      className="py-[1.3125rem] px-[1.5rem] rounded-default bg-gray-light placeholder:text-gray-normal"
+      {...props}
+    />
+  );
+});
+
 Input.displayName = 'Input';
-export default Input;
