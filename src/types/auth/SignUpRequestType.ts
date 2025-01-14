b5a94dffec28a2987dbff266721f7fab3cb3bb72
Cllaude99 <xodbsrla1025@naver.com>
2025-01-14 19:35:29 +0900

    feat: form 제출 타입과 비동기 request 타입을 분리해서 선언

--- src/types/auth/SignUpRequestType.ts
+++ src/types/auth/SignUpRequestType.ts
@@ -1,12 +1,15 @@
-import { ISignInRequest } from './SignInRequestType';
-
-export interface ISignUpRequest extends ISignInRequest {
-  confirmPw: string;
+export interface ISignUpRequest {
   name: string;
+  email: string;
+  pw: string;
   existAddress?: boolean;
   siDo?: string;
   siGunGu?: string;
   roadNameAddress?: string;
   addressLatitude?: number;
   addressLongitude?: number;
+}
+
+export interface ISignUpFormValues extends ISignUpRequest {
+  confirmPw: string;
 }
