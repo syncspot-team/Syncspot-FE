6aa02fe3b0584dd72b334a658948cec1e1338cf1
Cllaude99 <xodbsrla1025@naver.com>
2025-01-16 22:38:05 +0900

    feat: roomId 반영

--- src/state/queries/location/useGetPlaceSearchQuery.ts
+++ src/state/queries/location/useGetPlaceSearchQuery.ts
@@ -2,18 +2,20 @@ import { useQuery, UseQueryOptions } from '@tanstack/react-query';
 import { LOCATION_KEY } from './key';
 import { getPlaceSearch } from '@src/apis/location/getPlaceSearch';
 import { IPlaceSearchResponseType } from '@src/types/location/placeSearchResponseType';
+import { useParams } from 'react-router-dom';
 
 export const useGetPlaceSearchQuery = (
-  roomId: string,
   options?: UseQueryOptions<
     IPlaceSearchResponseType,
     Error,
     IPlaceSearchResponseType
   >,
 ) => {
+  const { roomId } = useParams();
+
   return useQuery({
-    queryKey: LOCATION_KEY.GET_PLACE_SEARCH(roomId),
-    queryFn: () => getPlaceSearch(roomId),
+    queryKey: LOCATION_KEY.GET_PLACE_SEARCH(roomId!),
+    queryFn: () => getPlaceSearch(roomId!),
     ...options,
   });
 };
