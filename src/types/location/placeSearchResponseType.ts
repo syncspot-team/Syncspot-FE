496f743b402fa6c222e9f197069865ea267299e0
Cllaude99 <xodbsrla1025@naver.com>
2025-01-16 13:22:24 +0900

    feat: 중간 지점 장소 입력 api 타입 설정

---
+++ src/types/location/placeSearchResponseType.ts
@@ -0,0 +1,21 @@
+interface ILocation {
+  placeId: number;
+  siDo: string;
+  siGunGu: string;
+  roadNameAddress: string;
+  addressLat: number;
+  addressLong: number;
+}
+
+interface IPlaceSearchData {
+  myLocationExistence: boolean;
+  myLocations: ILocation[];
+  friendLocationExistence: boolean;
+  friendLocations: ILocation[];
+}
+
+export interface IPlaceSearchResponseType {
+  isSuccess: boolean;
+  status: number;
+  data: IPlaceSearchData;
+}
