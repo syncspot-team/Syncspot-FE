408de53c24eb49122ed082ec6a3c8d42f50998aa
TaeYoon Kim <xodbsrla1025@naver.com>
2025-01-23 22:35:43 +0900

    Design/#97 (#98)
    * design: 공통 input 컴포넌트 디자인 수정
    
    * refactor: 장소 검색 결과가 아래에 나오도록 수정
    
    * design: input 컴포넌트 디자인 수정
    
    * design: 디자인 수정
    
    * feat: LocationEnterPage 복구

--- src/components/common/kakao/KakaoLocationPicker.tsx
+++ src/components/common/kakao/KakaoLocationPicker.tsx
@@ -1,8 +1,8 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { searchAddressInfo } from '@src/apis/kakao/searchAddressInfo';
-import KakaoLocationModal from '@src/components/common/modal/KakaoLocationModal';
-import KakaoLocationSearch from '@src/components/common/kakao/KakaoLocationSearch';
 import { Place, ISelectedLocation } from '@src/components/common/kakao/types';
+import { useDebounce } from '@src/hooks/useDebounce';
+import { searchPlacesByKeyword } from '@src/apis/kakao/searchPlacesByKeyword';
 import { mergeClassNames } from '@src/utils/mergeClassNames';
 
 interface IKakaoLocationPicker {
@@ -16,42 +16,74 @@ export default function KakaoLocationPicker({
   onSelect,
   defaultAddress,
 }: IKakaoLocationPicker) {
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const [selectedLocation, setSelectedLocation] =
-    useState<ISelectedLocation | null>(null);
+  const [searchTerm, setSearchTerm] = useState(defaultAddress || '');
+  const [suggestions, setSuggestions] = useState<Place[]>([]);
+  const [isSearching, setIsSearching] = useState(false);
+  const debouncedSearchTerm = useDebounce(searchTerm, 300);
 
   const handlePlaceSelect = async (place: Place) => {
+    setIsSearching(false);
     const addressData = await searchAddressInfo(place.road_address_name);
     const location = { place, address: addressData };
     const selectResult = onSelect?.(location);
-    if (selectResult !== false) {
-      setSelectedLocation(location);
+    if (selectResult) {
+      setSearchTerm(place.place_name);
     }
-
-    setIsModalOpen(false);
+    setSuggestions([]);
   };
 
-  return (
-    <>
-      <button
-        onClick={() => setIsModalOpen(true)}
-        className={mergeClassNames(
-          'cursor-pointer py-[1.3125rem] px-[1.5rem] rounded-default',
-          className,
-          !selectedLocation && !defaultAddress && 'text-gray-normal',
-        )}
-      >
-        {selectedLocation
-          ? selectedLocation.place.place_name
-          : defaultAddress || '장소를 선택해주세요'}
-      </button>
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchTerm(e.target.value);
+    setIsSearching(true);
+  };
 
-      <KakaoLocationModal
-        isOpen={isModalOpen}
-        onClose={() => setIsModalOpen(false)}
-      >
-        <KakaoLocationSearch onPlaceSelect={handlePlaceSelect} />
-      </KakaoLocationModal>
-    </>
+  const searchPlaces = async (query: string) => {
+    const documents = await searchPlacesByKeyword(query);
+    setSuggestions(documents);
+  };
+
+  useEffect(() => {
+    if (debouncedSearchTerm.trim() && isSearching) {
+      searchPlaces(debouncedSearchTerm);
+    } else {
+      setSuggestions([]);
+    }
+  }, [debouncedSearchTerm, isSearching]);
+
+  return (
+    <div className="relative">
+      <input
+        type="text"
+        value={searchTerm}
+        onChange={handleInputChange}
+        placeholder="장소를 선택해주세요"
+        className={mergeClassNames(
+          'w-full cursor-pointer py-[1.125rem] pr-[1.5rem] pl-[0.9375rem] rounded-default',
+          className,
+        )}
+      />
+
+      {isSearching && (
+        <div className="absolute z-50 w-full mt-[0.0625rem] rounded-md shadow-lg bg-white-default">
+          <div className="max-h-[25rem] overflow-y-auto scrollbar-thin scrollbar-thumb-gray-normal scrollbar-track-transparent scrollbar-thumb-rounded-full">
+            {suggestions.length > 0 ? (
+              suggestions.map((place) => (
+                <div
+                  key={place.id}
+                  onClick={() => handlePlaceSelect(place)}
+                  className="py-[1.125rem] pr-[1.5rem] pl-[0.9375rem] truncate rounded-md cursor-pointer hover:bg-gray-light"
+                >
+                  <div className="text-content">{place.place_name}</div>
+                </div>
+              ))
+            ) : (
+              <div className="p-4 text-center text-description text-black-default">
+                검색 결과가 존재하지 않습니다
+              </div>
+            )}
+          </div>
+        </div>
+      )}
+    </div>
   );
 }
