8fe950e4c3ba6abeefe8863cf2782e959b3d0c63
Cllaude99 <xodbsrla1025@naver.com>
2025-01-23 21:52:28 +0900

    design: input 컴포넌트 디자인 수정

--- src/components/common/input/Input.tsx
+++ src/components/common/input/Input.tsx
@@ -1,15 +1,21 @@
 import React, { forwardRef } from 'react';
+import { mergeClassNames } from '@src/utils/mergeClassNames';
 
 type InputProps = React.InputHTMLAttributes<HTMLInputElement>;
 
-export const Input = forwardRef<HTMLInputElement, InputProps>((props, ref) => {
-  return (
-    <input
-      ref={ref}
-      className="text-description py-[1.125rem] pr-[1.5rem] pl-[0.9375rem] rounded-default bg-gray-light placeholder:text-gray-normal"
-      {...props}
-    />
-  );
-});
+export const Input = forwardRef<HTMLInputElement, InputProps>(
+  ({ className, ...props }, ref) => {
+    return (
+      <input
+        ref={ref}
+        className={mergeClassNames(
+          'text-description py-[1.125rem] pl-[0.9375rem] pr-[1.5rem] rounded-default bg-gray-light placeholder:text-gray-normal',
+          className,
+        )}
+        {...props}
+      />
+    );
+  },
+);
 
 Input.displayName = 'Input';
