46dec5829e545d4e0338430a5a480a3db6588618
Cllaude99 <xodbsrla1025@naver.com>
2025-01-31 13:36:04 +0900

    design: 데스크탑 사이드 메뉴 디자인 수정

--- src/components/users/DesktopSideMenu.tsx
+++ src/components/users/DesktopSideMenu.tsx
@@ -3,10 +3,10 @@ import { sideMenuItems } from './constants/sideMenuItems';
 
 export default function DesktopSideMenu() {
   return (
-    <div className="bg-gray-light rounded-default p-3 lg:min-h-[calc(100vh-150px)]">
+    <div className="bg-gray-light rounded-default p-3 min-h-[calc(100vh-150px)]">
       {sideMenuItems.map((item) => (
         <div key={item.text}>
-          <div className="flex items-center gap-3 py-3 pl-4 font-semibold text-description text-gray-dark">
+          <div className="flex items-center gap-3 py-3 pl-4 font-bold text-description text-gray-dark">
             <span>{item.text}</span>
           </div>
           <div className="mb-3 ml-5">
