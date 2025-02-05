0719edd3ad9379304b628e63f1cc8ec7eb99e911
Cllaude99 <xodbsrla1025@naver.com>
2025-02-05 22:47:03 +0900

    fix: 로그아웃시에 방에 대한 store 값도 없어지도록 수정

--- src/state/store/roomStore.ts
+++ src/state/store/roomStore.ts
@@ -1,16 +1,24 @@
 import { create } from 'zustand';
 
 const SELECTED_ROOM_ID = 'selectedRoomId';
+const SELECTED_ROOM_NAME = 'selectedRoomName';
 
 interface IRoomIdState {
   roomId: string;
+  roomName: string;
   setRoomId: (roomId: string) => void;
+  setRoomName: (roomName: string) => void;
 }
 
 export const useRoomIdStore = create<IRoomIdState>((set) => ({
   roomId: localStorage.getItem(SELECTED_ROOM_ID) || '',
+  roomName: localStorage.getItem(SELECTED_ROOM_NAME) || '',
   setRoomId: (roomId: string) => {
     localStorage.setItem(SELECTED_ROOM_ID, roomId);
     set({ roomId });
+  },
+  setRoomName: (roomName: string) => {
+    localStorage.setItem(SELECTED_ROOM_NAME, roomName);
+    set({ roomName });
   },
 }));
