d3ea92bf8c5994e9cec9dec94a33df92d08c3c49
Cllaude99 <xodbsrla1025@naver.com>
2025-02-06 16:23:13 +0900

    feat: mutation에 따른 invalidate 과정 추가

--- src/state/mutations/onboarding/usePatchRoomMemoMutation.ts
+++ src/state/mutations/onboarding/usePatchRoomMemoMutation.ts
@@ -1,12 +1,24 @@
 import { patchRoomMemo } from '@src/apis/onboarding/patchRoomMemo';
+import { ROOM_QUERY_KEY } from '@src/state/queries/header/key';
 import { IPatchRoomMemoRequestType } from '@src/types/onboarding/patchRoomMemoRequestType';
-import { useMutation, UseMutationOptions } from '@tanstack/react-query';
+import {
+  useMutation,
+  UseMutationOptions,
+  useQueryClient,
+} from '@tanstack/react-query';
 
 export const usePatchRoomMemoMutation = (
   options?: UseMutationOptions<any, Error, IPatchRoomMemoRequestType>,
 ) => {
+  const queryClient = useQueryClient();
+
   return useMutation({
     mutationFn: patchRoomMemo,
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ROOM_QUERY_KEY.GET_JOINED_ROOM(),
+      });
+    },
     ...options,
   });
 };
