abfbfe09033a87a124b861e16a1178f5d2df3338
Cllaude99 <xodbsrla1025@naver.com>
2025-02-08 22:05:06 +0900

    feat: key파일 추가

--- src/state/queries/location/key.ts
+++ src/state/queries/location/key.ts
@@ -1,3 +1,8 @@
 export const LOCATION_KEY = {
   GET_PLACE_SEARCH: (roomId: string) => ['getPlaceSearch', roomId],
+  GET_MIDPOINT_SEARCH: (roomId: string) => ['getMidpointSearch', roomId],
+  GET_MIDPOINT_TIME_SEARCH: (roomId: string) => [
+    'getMidpointTimeSearch',
+    roomId,
+  ],
 };
