b7274613579db2bef372e16fab87690d9f5db4f0
Cllaude99 <xodbsrla1025@naver.com>
2025-02-08 23:18:45 +0900

    feat: 중간 지점 결과 화면 api 연동

--- src/pages/location/LocationResultPage.tsx
+++ src/pages/location/LocationResultPage.tsx
@@ -4,138 +4,85 @@ import IconRightHalfArrow from '@src/assets/icons/IconRightHalfArrow.svg?react';
 import IconLinkPin from '@src/assets/icons/IconLinkPin.svg?react';
 import { useNavigate, useParams } from 'react-router-dom';
 import { PATH } from '@src/constants/path';
+import { useMidpointSearchQuery } from '@src/state/queries/location/useMidpointSearchQuery';
+import { useGetPlaceSearchQuery } from '@src/state/queries/location/useGetPlaceSearchQuery';
+import { IMidpointDataResponseType } from '@src/types/location/midpointSearchResponseType';
+import { useMidpointTimeSearchQuery } from '@src/state/queries/location/useMidpointTimeSearchQuery';
+import SomethingWrongErrorPage from '../error/SomethingWrongErrorPage';
 
-const ENTER_LOCATIONS = [
-  {
-    placeId: 1,
-    siDo: '서울특별시',
-    siGunGu: '강남구',
-    roadNameAddress: '테헤란로 427',
-    addressLat: 37.5065,
-    addressLong: 127.0536,
-  },
-  {
-    placeId: 2,
-    siDo: '서울특별시',
-    siGunGu: '서초구',
-    roadNameAddress: '강남대로 373',
-    addressLat: 37.4969,
-    addressLong: 127.0278,
-  },
-  {
-    placeId: 3,
-    siDo: '서울특별시',
-    siGunGu: '마포구',
-    roadNameAddress: '월드컵북로 396',
-    addressLat: 37.5826,
-    addressLong: 126.9012,
-  },
-  {
-    placeId: 4,
-    siDo: '서울특별시',
-    siGunGu: '용산구',
-    roadNameAddress: '이태원로 217',
-    addressLat: 37.5384,
-    addressLong: 126.9946,
-  },
-  {
-    placeId: 5,
-    siDo: '서울특별시',
-    siGunGu: '송파구',
-    roadNameAddress: '올림픽로 300',
-    addressLat: 37.5154,
-    addressLong: 127.1039,
-  },
-];
-
-const RESULT_LOCATIONS = [
-  {
-    placeId: 1,
-    siDo: '서울특별시',
-    siGunGu: '노원구',
-    roadNameAddress: '동일로 1238',
-    addressLat: 37.6544,
-    addressLong: 127.0565,
-  },
-  {
-    placeId: 2,
-    siDo: '서울특별시',
-    siGunGu: '구로구',
-    roadNameAddress: '디지털로 300',
-    addressLat: 37.484,
-    addressLong: 126.9011,
-  },
-  {
-    placeId: 3,
-    siDo: '서울특별시',
-    siGunGu: '중구',
-    roadNameAddress: '을지로 50',
-    addressLat: 37.5657,
-    addressLong: 126.9839,
-  },
-  {
-    placeId: 4,
-    siDo: '서울특별시',
-    siGunGu: '강북구',
-    roadNameAddress: '한천로 660',
-    addressLat: 37.6397,
-    addressLong: 127.0266,
-  },
-  {
-    placeId: 5,
-    siDo: '서울특별시',
-    siGunGu: '광진구',
-    roadNameAddress: '능동로 209',
-    addressLat: 37.5472,
-    addressLong: 127.0744,
-  },
-];
-
-interface ILocation {
-  placeId: number;
-  siDo: string;
-  siGunGu: string;
+interface Coordinate {
+  lat: number;
+  lng: number;
+  isMyLocation: boolean;
   roadNameAddress: string;
-  addressLat: number;
-  addressLong: number;
+  name: string;
+  siGunGu: string;
+  isSelected?: boolean;
 }
 
 export default function LocationResultPage() {
   const SEQUENCE = ['첫', '두', '세', '네', '다섯'];
   const navigate = useNavigate();
+  const [selectedLocationIndex, setSelectedLocationIndex] = useState<number>(0);
   const { roomId } = useParams();
-  const [selectedLocation, setSelectedLocation] = useState<number>(
-    RESULT_LOCATIONS[0].placeId,
-  );
 
-  const coordinates = useMemo(() => {
-    const enterCoords = ENTER_LOCATIONS.map((location) => ({
-      lat: location.addressLat,
-      lng: location.addressLong,
-      isMyLocation: false,
-      roadNameAddress: location.roadNameAddress,
-    }));
+  const { data: placeSearchData } = useGetPlaceSearchQuery();
+  const { data: midpointSearchData } = useMidpointSearchQuery();
 
-    const selectedCoord = RESULT_LOCATIONS.filter(
-      (location) => location.placeId === selectedLocation,
-    ).map((location) => ({
-      lat: location.addressLat,
-      lng: location.addressLong,
-      isMyLocation: true,
-      roadNameAddress: location.roadNameAddress,
-    }));
+  const selectedMidpoint = midpointSearchData?.data[selectedLocationIndex];
+  const { data: timeSearchData, isLoading: isTimeSearchLoading } =
+    useMidpointTimeSearchQuery(
+      selectedMidpoint?.addressLat || 0,
+      selectedMidpoint?.addressLong || 0,
+      {
+        enabled:
+          !!selectedMidpoint &&
+          selectedMidpoint.addressLat !== 0 &&
+          selectedMidpoint.addressLong !== 0,
+      },
+    );
 
-    return [...enterCoords, ...selectedCoord];
-  }, [selectedLocation]);
+  const coordinates = useMemo<Coordinate[]>(() => {
+    if (!placeSearchData?.data || !midpointSearchData?.data) return [];
 
-  const handleClickMidpoint = (location: ILocation) => {
+    const myLocations = placeSearchData.data.myLocationExistence
+      ? [
+          {
+            lat: placeSearchData.data.myLocations[0].addressLat,
+            lng: placeSearchData.data.myLocations[0].addressLong,
+            isMyLocation: true,
+            roadNameAddress:
+              placeSearchData.data.myLocations[0].roadNameAddress,
+            name: '내 위치',
+            siGunGu: '',
+          },
+        ]
+      : [];
+
+    const midpointLocations = midpointSearchData.data.map(
+      (location: IMidpointDataResponseType, index: number) => ({
+        lat: location.addressLat,
+        lng: location.addressLong,
+        isMyLocation: false,
+        roadNameAddress: location.name || '위치 정보 없음',
+        name: location.name,
+        siGunGu: `${location.siDo} ${location.siGunGu}`,
+        isSelected: index === selectedLocationIndex,
+      }),
+    );
+
+    return [...myLocations, ...midpointLocations];
+  }, [selectedLocationIndex, placeSearchData, midpointSearchData]);
+
+  const handleClickMidpoint = (location: IMidpointDataResponseType) => {
     const { addressLat, addressLong, roadNameAddress } = location;
-
     navigate(
       PATH.LOCATION_RECOMMENDATIONS(roomId) +
         `?lat=${addressLat}&lng=${addressLong}&location=${roadNameAddress}`,
     );
   };
+
+  if (!midpointSearchData) return <SomethingWrongErrorPage />;
 
   return (
     <div className="grid w-full grid-cols-1 lg:grid-cols-10 px-4 lg:px-[7.5rem] gap-[0.9375rem] mt-[1.5625rem]">
@@ -144,49 +91,69 @@ export default function LocationResultPage() {
       </div>
       <div className="lg:col-span-4">
         <ul className="grid grid-cols-1 grid-rows-5 h-full gap-[0.625rem]">
-          {RESULT_LOCATIONS.map((location, index) => (
-            <li
-              key={location.placeId}
-              className={`flex flex-col justify-center h-full max-h-[8.125rem] p-4 cursor-pointer rounded-default shadow-sm ${
-                selectedLocation === location.placeId
-                  ? 'bg-blue-100 opacity-95 ring-2 ring-blue-normal01'
-                  : 'bg-gray-light'
-              }`}
-              onClick={() => setSelectedLocation(location.placeId)}
-            >
-              <div className="flex items-center justify-between">
-                <span className="flex items-center gap-2">
-                  <span className="flex items-center justify-center -mt-[0.0625rem] rounded-full text-white-default bg-blue-dark01 size-6">
-                    {index + 1}
+          {midpointSearchData.data.map(
+            (location: IMidpointDataResponseType, index: number) => (
+              <li
+                key={index}
+                className={`flex flex-col justify-center h-full max-h-[140px] p-4 cursor-pointer rounded-default shadow-sm ${
+                  selectedLocationIndex === index
+                    ? 'bg-blue-100 opacity-95 ring-2 ring-blue-normal01'
+                    : 'bg-gray-light'
+                }`}
+                onClick={() => setSelectedLocationIndex(index)}
+              >
+                <div className="flex items-center justify-between">
+                  <span className="flex items-center gap-2">
+                    <span className="flex items-center justify-center -mt-[0.0625rem] rounded-full text-white-default bg-blue-dark01 size-6">
+                      {index + 1}
+                    </span>
+                    <span className="text-blue-dark02 text-content">
+                      우리의 {SEQUENCE[index]} 번째 중간 지점
+                    </span>
                   </span>
-                  <span className="text-blue-dark02 text-content">
-                    우리의 {SEQUENCE[index]} 번째 중간 지점
+                  <IconRightHalfArrow
+                    onClick={(e) => {
+                      e.stopPropagation();
+                      handleClickMidpoint(location);
+                    }}
+                    className={`${
+                      selectedLocationIndex === index
+                        ? 'opacity-100'
+                        : 'opacity-0'
+                    } rounded-[0.4375rem] p-1 mr-2 text-primary hover:bg-gray-light hover:scale-110`}
+                  />
+                </div>
+                <div className="flex items-center gap-4 my-1">
+                  <span className="truncate text-blue-dark01 text-subtitle">
+                    {location.name}
                   </span>
-                </span>
-                <IconRightHalfArrow
-                  onClick={() => {
-                    handleClickMidpoint(location);
-                  }}
-                  className={`${
-                    selectedLocation === location.placeId
-                      ? 'opacity-100'
-                      : 'opacity-0'
-                  } rounded-[0.4375rem] p-1 mr-2 text-primary hover:bg-gray-light hover:scale-110`}
-                />
-              </div>
-              <div className="flex items-center gap-4 my-[0.375rem]">
-                <span className="truncate text-blue-dark01 text-subtitle">
-                  {location.roadNameAddress}
-                </span>
-                <IconLinkPin className="flex-shrink-0 size-[1.125rem]" />
-              </div>
-              <div className="flex items-center gap-2">
-                <span className="truncate text-gray-dark">
-                  {location.siGunGu}
-                </span>
-              </div>
-            </li>
-          ))}
+                  <IconLinkPin className="flex-shrink-0 size-[1.125rem]" />
+                </div>
+                {selectedLocationIndex === index ? (
+                  <div className="flex flex-col gap-1">
+                    {!isTimeSearchLoading &&
+                      timeSearchData?.data?.elements[0] && (
+                        <div className="flex gap-2">
+                          <span className="px-2 py-1 rounded-md bg-primary text-description text-white-default">
+                            {timeSearchData.data.elements[0].distance.text}
+                          </span>
+                          <span className="px-2 py-1 rounded-md bg-primary text-description text-white-default">
+                            {timeSearchData.data.elements[0].duration.text}
+                          </span>
+                        </div>
+                      )}
+                    <span className="truncate text-gray-dark">
+                      {location.roadNameAddress || '위치 정보 없음'}
+                    </span>
+                  </div>
+                ) : (
+                  <span className="mt-1 truncate text-gray-dark">
+                    {location.roadNameAddress || '위치 정보 없음'}
+                  </span>
+                )}
+              </li>
+            ),
+          )}
         </ul>
       </div>
     </div>
