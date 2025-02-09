87ebe6a8b29f5727cc7dca649c5fff0e7f914989
Cllaude99 <xodbsrla1025@naver.com>
2025-02-09 14:09:57 +0900

    feat: 장소 추천 결과 화면 invalidate 추가

--- src/state/mutations/location/usePlaceUpdateMutation.ts
+++ src/state/mutations/location/usePlaceUpdateMutation.ts
@@ -28,6 +28,9 @@ export const usePlaceUpdateMutation = (
       queryClient.invalidateQueries({
         queryKey: LOCATION_KEY.GET_MIDPOINT_SEARCH(roomId!),
       });
+      queryClient.invalidateQueries({
+        queryKey: LOCATION_KEY.GET_RECOMMEND_PLACE_SEARCH(roomId!),
+      });
     },
     ...options,
   });
