d9ddb3d7f169b2b421a4f69067a85d9e68eae677
“chae-dahee” <cdh010126r@gmail.com>
2025-02-11 15:27:30 +0900

    fix: timeSelectType 상수 처리

--- src/components/voteTime/timeSelect.tsx
+++ src/components/voteTime/timeSelect.tsx
@@ -4,8 +4,15 @@ import { mergeClassNames } from '@src/utils/mergeClassNames';
 import IconTimeDropdown from '@src/assets/icons/IconTimeDropdown.svg?react';
 import { formatTime } from '@src/utils/formatTime';
 
+const SELECT_TYPES = {
+  HOUR: 'hour' as const,
+  MINUTE: 'minute' as const,
+};
+
+type SelectType = (typeof SELECT_TYPES)[keyof typeof SELECT_TYPES];
+
 type SelectProps = React.SelectHTMLAttributes<HTMLSelectElement> & {
-  selectType: 'hour' | 'minute';
+  selectType: SelectType;
 };
 
 export const Select = forwardRef<HTMLSelectElement, SelectProps>(
@@ -18,14 +25,14 @@ export const Select = forwardRef<HTMLSelectElement, SelectProps>(
           {...props}
         >
           {/* selectType hour일때 */}
-          {selectType === 'hour' &&
+          {selectType === SELECT_TYPES.HOUR &&
             Array.from({ length: 24 }, (_, index) => (
               <option key={index} value={index} className="bg-white">
                 {formatTime(index)} 시
               </option>
             ))}
           {/* selectType minute일때 */}
-          {selectType === 'minute' &&
+          {selectType === SELECT_TYPES.MINUTE &&
             Array.from({ length: 6 }, (_, index) => (
               <option key={index} value={index * 10} className="bg-white">
                 {formatTime(index * 10)} 분
