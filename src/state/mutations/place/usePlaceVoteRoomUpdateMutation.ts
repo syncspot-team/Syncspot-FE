5abdb2038a55f6b6e20d166d0a45b6d7f27ee567
Cllaude99 <xodbsrla1025@naver.com>
2025-02-12 15:55:55 +0900

    fix: 장소 투표 재생성 후에 장소 목록을 불러오는 쿼리에 대한 invalidate 추가

--- src/state/mutations/place/usePlaceVoteRoomUpdateMutation.ts
+++ src/state/mutations/place/usePlaceVoteRoomUpdateMutation.ts
@@ -1,16 +1,27 @@
 import { putPlaceVoteRoomUpdate } from '@src/apis/place/putPlaceVoteRoomUpdate';
+import { PLACE_VOTE_ROOM_KEY } from '@src/state/queries/place/key';
 import { IPlaceVoteRoomUpdateRequestType } from '@src/types/place/placeVoteRoomUpdateRequestType';
-import { useMutation, UseMutationOptions } from '@tanstack/react-query';
+import {
+  useMutation,
+  UseMutationOptions,
+  useQueryClient,
+} from '@tanstack/react-query';
 import { useParams } from 'react-router-dom';
 
 export const usePlaceVoteRoomUpdateMutation = (
   options?: UseMutationOptions<any, Error, IPlaceVoteRoomUpdateRequestType>,
 ) => {
+  const queryClient = useQueryClient();
   const { roomId } = useParams();
 
   return useMutation({
     mutationFn: (placeVoteRoomUpdatePayload: IPlaceVoteRoomUpdateRequestType) =>
       putPlaceVoteRoomUpdate(roomId!, placeVoteRoomUpdatePayload),
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: PLACE_VOTE_ROOM_KEY.GET_PLACE_VOTE_ROOM_CHECK(roomId!),
+      });
+    },
     ...options,
   });
 };
