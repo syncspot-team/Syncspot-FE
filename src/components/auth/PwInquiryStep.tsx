56db40e11b7d775f31dd0a4a83f34a4ec37a07ce
Cllaude99 <xodbsrla1025@naver.com>
2025-02-13 18:47:31 +0900

    feat: 비밀번호 찾기 api 연동

--- src/components/auth/PwInquiryStep.tsx
+++ src/components/auth/PwInquiryStep.tsx
@@ -1,8 +1,9 @@
 import { useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { Input } from '@src/components/common/input/Input';
-import Button from '@src/components/common/button/Button';
 import { PwInquiryStepType } from '@src/types/auth/PwInquiryStepType';
+import { usePwReissueMutation } from '@src/state/mutations/auth/usePwReissueMutation';
+import { usePwReissueEmailVerificationMutation } from '@src/state/mutations/auth/usePwReissueEmailVerificationMutation';
 
 interface IFormValues {
   email: string;
@@ -11,15 +12,16 @@ interface IFormValues {
 
 interface IPwInquiryStepProps {
   setPwInquiryStep: (step: keyof typeof PwInquiryStepType) => void;
-  setTempPassword: (password: string) => void;
 }
 
 export default function PwInquiryStep({
   setPwInquiryStep,
-  setTempPassword,
 }: IPwInquiryStepProps) {
   const [isEmailSent, setIsEmailSent] = useState(false);
-  const [isVerified, setIsVerified] = useState(false);
+  const { mutate: requestEmailVerification } =
+    usePwReissueEmailVerificationMutation();
+  const { mutate: reissue } = usePwReissueMutation();
+
   const {
     register,
     watch,
@@ -28,13 +30,73 @@ export default function PwInquiryStep({
     formState: { errors },
   } = useForm<IFormValues>();
 
-  const handleEmailSubmit = async (email: string) => {
+  return (
+    <div className="flex flex-col w-full max-w-[28.125rem]">
+      <h3 className="ml-2 mb-[0.125rem] text-content lg:text-menu text-tertiary">
+        아이디 (이메일)
+      </h3>
+      <div className="relative flex gap-1">
+        <Input
+          {...register('email')}
+          placeholder="이메일을 입력해주세요"
+          className="w-full text-description"
+        />
+        <button
+          onClick={isEmailSent ? handleResendEmail : handleEmailSubmit}
+          disabled={!watch('email')}
+          className="absolute right-3 top-1/2 -translate-y-1/2 h-[2.125rem] whitespace-nowrap text-description bg-gray-normal p-2 text-white-default rounded-md hover:enabled:bg-gray-dark cursor-pointer disabled:cursor-not-allowed"
+        >
+          {isEmailSent ? '재전송' : '인증코드 받기'}
+        </button>
+      </div>
+      {errors.email && (
+        <p className="mt-3 text-center text-description text-error-normal">
+          {errors.email.message}
+        </p>
+      )}
+
+      {isEmailSent && (
+        <>
+          <h3 className="ml-2 mt-6 mb-[0.125rem] text-content lg:text-menu text-tertiary">
+            인증코드
+          </h3>
+          <div className="relative flex gap-1">
+            <Input
+              {...register('code')}
+              placeholder="이메일로 발송된 인증 코드를 입력해주세요"
+              disabled={!isEmailSent}
+              className="w-full disabled:cursor-not-allowed text-description"
+            />
+            <button
+              onClick={() =>
+                handleVerificationSubmit({
+                  email: watch('email'),
+                  code: watch('code'),
+                })
+              }
+              disabled={!isEmailSent}
+              className="absolute right-3 top-1/2 -translate-y-1/2 h-[2.125rem] whitespace-nowrap text-description bg-gray-normal p-2 text-white-default rounded-md hover:enabled:bg-gray-dark cursor-pointer disabled:cursor-not-allowed"
+            >
+              확인
+            </button>
+          </div>
+          {errors.code && (
+            <p className="mt-3 text-center text-description text-error-normal">
+              {errors.code.message}
+            </p>
+          )}
+        </>
+      )}
+    </div>
+  );
+
+  function validateEmail(email: string) {
     if (!email) {
       setError('email', {
         type: 'required',
         message: '이메일을 입력해주세요.',
       });
-      return;
+      return false;
     }
 
     const emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
@@ -43,40 +105,38 @@ export default function PwInquiryStep({
         type: 'pattern',
         message: '올바른 이메일 형식이 아닙니다.',
       });
-      return;
+      return false;
     }
 
-    // 이메일 인증 API 호출하는 과정 필요
-    clearErrors('email');
-    setIsEmailSent(true);
-  };
+    return true;
+  }
 
-  const handleResendEmail = () => {
+  function handleEmailSubmit() {
     const email = watch('email');
+    if (!validateEmail(email)) return;
+    clearErrors('email');
 
-    if (!email) {
-      setError('email', {
-        type: 'required',
-        message: '이메일을 입력해주세요.',
-      });
-      return;
-    }
+    requestEmailVerification(
+      { email },
+      {
+        onSuccess: () => {
+          setIsEmailSent(true);
+        },
+      },
+    );
+  }
 
-    const emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
-    if (!emailRegex.test(email)) {
-      setError('email', {
-        type: 'pattern',
-        message: '올바른 이메일 형식이 아닙니다.',
-      });
-      return;
-    }
+  function handleResendEmail() {
+    const email = watch('email');
+    if (!validateEmail(email)) return;
+    clearErrors('email');
 
     if (window.confirm('재전송하시겠습니까?')) {
-      handleEmailSubmit(email);
+      handleEmailSubmit();
     }
-  };
+  }
 
-  const handleVerificationSubmit = async (data: IFormValues) => {
+  function handleVerificationSubmit(data: IFormValues) {
     if (!data.code) {
       setError('code', {
         type: 'required',
@@ -85,114 +145,21 @@ export default function PwInquiryStep({
       return;
     }
 
-    // 인증코드 검증 API 호출하는 과정 필요
-    const isSuccess = true; // API 응답값으로 대체 필요
+    clearErrors('code');
 
-    if (isSuccess) {
-      clearErrors('code');
-      setIsVerified(true);
-    } else {
-      setError('code', {
-        type: 'validate',
-        message: '인증코드가 다릅니다.',
-      });
-    }
-  };
-
-  const handleTempPasswordRequest = async () => {
-    // 임시 비밀번호 발급 API 호출
-    setTempPassword('123456'); // 부모 컴포넌트로 임시 비밀번호 전달
-    setPwInquiryStep(PwInquiryStepType.PW_REISSUE_STEP);
-  };
-
-  const renderVerificationMessage = () => {
-    if (errors.code) {
-      return (
-        <p className="flex items-center justify-center mt-1 ml-2 text-description lg:text-content text-error-normal">
-          {errors.code.message}
-        </p>
-      );
-    }
-
-    if (isVerified) {
-      return (
-        <p className="flex items-center justify-center mt-1 ml-2 text-description lg:text-content text-blue-dark02">
-          인증 코드가 확인되었습니다!
-        </p>
-      );
-    }
-
-    return null;
-  };
-
-  return (
-    <div className="flex flex-col w-full max-w-[28.125rem]">
-      <h3 className="ml-2 mb-[0.125rem] text-content lg:text-menu text-tertiary">
-        아이디 (이메일)
-      </h3>
-      <div className="relative flex gap-1">
-        <Input
-          {...register('email', {
-            required: '이메일을 입력해주세요.',
-            pattern: {
-              value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
-              message: '올바른 이메일 형식이 아닙니다.',
-            },
-          })}
-          placeholder="이메일을 입력해주세요"
-          className="w-full text-description"
-        />
-        <button
-          onClick={
-            isEmailSent
-              ? handleResendEmail
-              : () => handleEmailSubmit(watch('email'))
-          }
-          disabled={!watch('email')}
-          className="absolute right-3 top-1/2 -translate-y-1/2 h-[2.125rem] whitespace-nowrap text-description bg-gray-normal p-2 text-white-default rounded-md hover:enabled:bg-gray-dark cursor-pointer disabled:cursor-not-allowed"
-        >
-          {isEmailSent ? '재전송' : '인증코드 받기'}
-        </button>
-      </div>
-      {errors.email && (
-        <p className="mt-1 ml-2 text-description lg:text-content text-error-normal">
-          {errors.email.message}
-        </p>
-      )}
-
-      <h3 className="ml-2 mt-6 mb-[0.125rem] text-content lg:text-menu text-tertiary">
-        인증코드
-      </h3>
-      <div className="relative flex gap-1">
-        <Input
-          {...register('code')}
-          placeholder="이메일로 발송된 인증 코드를 입력해주세요"
-          disabled={!isEmailSent}
-          className="w-full disabled:cursor-not-allowed text-description"
-        />
-        <button
-          onClick={() =>
-            handleVerificationSubmit({
-              email: watch('email'),
-              code: watch('code'),
-            })
-          }
-          disabled={!isEmailSent}
-          className="absolute right-3 top-1/2 -translate-y-1/2 h-[2.125rem] whitespace-nowrap text-description bg-gray-normal p-2 text-white-default rounded-md hover:enabled:bg-gray-dark cursor-pointer disabled:cursor-not-allowed"
-        >
-          확인
-        </button>
-      </div>
-      {renderVerificationMessage()}
-      <Button
-        buttonType="primary"
-        onClick={handleTempPasswordRequest}
-        isLoading={false}
-        disabled={!isVerified}
-        className="w-full px-5 mt-10"
-      >
-        임시 비밀번호 발급
-      </Button>
-    </div>
-  );
+    reissue(
+      { email: data.email, code: data.code },
+      {
+        onSuccess: () => {
+          setPwInquiryStep(PwInquiryStepType.PW_REISSUE_STEP);
+        },
+        onError: () => {
+          setError('code', {
+            type: 'validate',
+            message: '인증코드가 다릅니다.',
+          });
+        },
+      },
+    );
+  }
 }
--- src/pages/auth/HelpPwInquiryPage.tsx
+++ src/pages/auth/HelpPwInquiryPage.tsx
@@ -7,28 +7,20 @@ export default function HelpPwInquiryPage() {
   const [pwInquiryStep, setPwInquiryStep] = useState<
     keyof typeof PwInquiryStepType
   >(PwInquiryStepType.PW_INQUIRY_STEP);
-  const [tempPassword, setTempPassword] = useState('');
 
   return (
     <div className="flex flex-col items-center justify-center mx-auto my-0 mt-[8.75rem] px-4 lg:px-[7.5rem]">
-      <h1
-        className={`mb-8 text-subtitle lg:text-title text-tertiary ${pwInquiryStep === PwInquiryStepType.PW_REISSUE_STEP && 'mt-12'}`}
-      >
+      <h1 className="mb-8 text-subtitle lg:text-title text-tertiary">
         {pwInquiryStep === PwInquiryStepType.PW_INQUIRY_STEP
           ? '비밀번호 찾기'
           : '비밀번호 재발급 안내'}
       </h1>
 
       {pwInquiryStep === PwInquiryStepType.PW_INQUIRY_STEP && (
-        <PwInquiryStep
-          setPwInquiryStep={setPwInquiryStep}
-          setTempPassword={setTempPassword}
-        />
+        <PwInquiryStep setPwInquiryStep={setPwInquiryStep} />
       )}
 
-      {pwInquiryStep === PwInquiryStepType.PW_REISSUE_STEP && (
-        <PwReissueStep tempPassword={tempPassword} />
-      )}
+      {pwInquiryStep === PwInquiryStepType.PW_REISSUE_STEP && <PwReissueStep />}
     </div>
   );
 }
