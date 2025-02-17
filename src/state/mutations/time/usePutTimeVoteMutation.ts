0a98a135becc3600a38cffca3759c2e738039a39
“chae-dahee” <cdh010126r@gmail.com>
2025-02-17 19:43:55 +0900

    feat: onSuccess, invalidate

--- src/state/mutations/time/usePutTimeVoteMutation.ts
+++ src/state/mutations/time/usePutTimeVoteMutation.ts
@@ -1,19 +1,33 @@
-import { useMutation, UseMutationOptions } from '@tanstack/react-query';
-import { useParams } from 'react-router-dom';
+import {
+  useMutation,
+  UseMutationOptions,
+  useQueryClient,
+} from '@tanstack/react-query';
+import { useNavigate, useParams } from 'react-router-dom';
 import { putTimeVote } from '@src/apis/time/putTimeVote';
 import {
   ITimeVoteRequest,
   ITimeVoteResponse,
 } from '@src/types/time/timeVoteType';
+import { PATH } from '@src/constants/path';
+import { TIME_KEY } from '@src/state/queries/time/key';
 
 export const usePutTimeVoteMutation = (
   options?: UseMutationOptions<ITimeVoteResponse, Error, ITimeVoteRequest>,
 ) => {
   const { roomId } = useParams();
+  const queryClient = useQueryClient();
+  const navigate = useNavigate();
 
   return useMutation({
     mutationFn: ({ dateTime }: ITimeVoteRequest) =>
       putTimeVote({ roomId, dateTime }),
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: TIME_KEY.GET_TIME_VOTED(roomId!),
+      });
+      navigate(PATH.TIME_RESULT(roomId));
+    },
     ...options,
   });
 };
--- src/state/queries/time/useGetTimeResultQuery.ts
+++ src/state/queries/time/useGetTimeResultQuery.ts
@@ -1,4 +1,8 @@
-import { useQuery, UseQueryOptions } from '@tanstack/react-query';
+import {
+  useQuery,
+  useQueryClient,
+  UseQueryOptions,
+} from '@tanstack/react-query';
 import { useParams } from 'react-router-dom';
 import { TIME_KEY } from './key';
 import { ITimeResultResponseType } from '@src/types/time/timeResultType';
@@ -12,6 +16,11 @@ export const useGetTimeResultQuery = (
   >,
 ) => {
   const { roomId } = useParams();
+  const queryClient = useQueryClient();
+
+  queryClient.invalidateQueries({
+    queryKey: TIME_KEY.GET_TIME_RESULT(roomId!),
+  });
 
   return useQuery({
     queryKey: TIME_KEY.GET_TIME_RESULT(roomId!),
