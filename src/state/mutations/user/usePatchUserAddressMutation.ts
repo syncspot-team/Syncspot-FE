5ed5418bb21ffc2c4041f9c78cfbf2de929a7f8c
Cllaude99 <xodbsrla1025@naver.com>
2025-02-18 11:41:30 +0900

    feat: 닉네임 및 주소 수정 api 연동

--- src/state/mutations/user/usePatchUserAddressMutation.ts
+++ src/state/mutations/user/usePatchUserAddressMutation.ts
@@ -1,12 +1,22 @@
-import { useMutation, UseMutationOptions } from '@tanstack/react-query';
+import {
+  useMutation,
+  UseMutationOptions,
+  useQueryClient,
+} from '@tanstack/react-query';
 import { patchUserAddress } from '@src/apis/users/patchUserAddress';
 import { IModifyUserAddressRequest } from '@src/types/users/modifyUserAddressRequestType';
+import { USER_QUERY_KEY } from '@src/state/queries/users/key';
 
 export const usePatchUserAddressMutation = (
   options?: UseMutationOptions<any, Error, IModifyUserAddressRequest>,
 ) => {
+  const queryClient = useQueryClient();
+
   return useMutation({
     mutationFn: patchUserAddress,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: USER_QUERY_KEY.USER_INFO });
+    },
     ...options,
   });
 };
