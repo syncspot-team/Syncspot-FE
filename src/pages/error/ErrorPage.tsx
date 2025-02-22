79453e2070f9eba0873006f7bc8d61e2984063af
Cllaude99 <xodbsrla1025@naver.com>
2025-02-23 02:34:16 +0900

    design: mt 수정

--- src/pages/error/ErrorPage.tsx
+++ src/pages/error/ErrorPage.tsx
@@ -41,12 +41,16 @@ const ErrorPage = ({
 
   return (
     <Layout>
-      <div className="flex flex-col gap-2 items-center justify-center h-[calc(100dvh-20rem)] mt-16">
+      <div
+        className={`flex flex-col gap-2 items-center justify-center h-[calc(100dvh-20rem)] ${
+          !isUnknownError ? 'mt-16' : 'mt-14'
+        }`}
+      >
         <Lottie animationData={Lottie404} className="size-52 lg:size-96" />
-        <div className="grid grid-cols-2 gap-2">
+        <div className="grid grid-cols-2 gap-2 lg:-mt-5">
           <button
             onClick={handleHomeClick}
-            className="p-2 rounded-md lg:p-3 bg-blue-normal01 text-white-default hover:bg-blue-normal02 text-description lg:text-content"
+            className="p-2 rounded-md lg:p-3 bg-blue-normal01 ring-2 ring-blue-normal01 hover:ring-blue-normal02 text-white-default hover:bg-blue-normal02 text-description lg:text-content"
           >
             홈으로 돌아가기
           </button>
@@ -57,9 +61,14 @@ const ErrorPage = ({
           ) : (
             <Link
               to={'https://github.com/Cotato-Syncspot/Syncspot-FE'}
-              className="flex items-center justify-center p-2 rounded-md lg:p-3 bg-white-default ring-2 ring-blue-normal01 text-blue-normal01 hover:bg-gray-light text-description lg:text-content"
+              className="flex items-center justify-center gap-2 p-2 rounded-md lg:p-3 bg-black-default text-white-default ring-2 ring-black-default hover:opacity-80 text-description lg:text-content"
             >
-              에러 문의하기
+              <img
+                src={IconGithub}
+                alt="github"
+                className="size-5 lg:size-6 -mt-[1px]"
+              />
+              <span>에러 문의하기</span>
             </Link>
           )}
           {!isUnknownError && (
