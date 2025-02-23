9a9bb970371093cf8afafb373ef935ba7ae1ae6f
Cllaude99 <xodbsrla1025@naver.com>
2025-02-23 18:56:54 +0900

    chore: global에서 input 자동완성 디자인 처리하도록 변경

--- src/components/common/input/Input.tsx
+++ src/components/common/input/Input.tsx
@@ -10,8 +10,6 @@ export const Input = forwardRef<HTMLInputElement, InputProps>(
         ref={ref}
         className={mergeClassNames(
           'text-description py-[1.125rem] pl-[0.9375rem] rounded-default bg-gray-light placeholder:text-gray-normal',
-          '[&:-webkit-autofill]:bg-gray-light [&:-webkit-autofill]:hover:bg-gray-light [&:-webkit-autofill]:focus:bg-gray-light [&:-webkit-autofill]:active:bg-gray-light',
-          '[&:-webkit-autofill]:[transition:none] [&:-webkit-autofill]:[box-shadow:0_0_0_30px_theme(colors.gray.light)_inset]',
           className,
         )}
         {...props}
