e6722132e9145c95fde8f5c4da7a2fa63ecfab38
“chae-dahee” <cdh010126r@gmail.com>
2025-03-01 00:09:53 +0900

    feat: 캘린더 모달

--- src/types/modalType.ts
+++ src/types/modalType.ts
@@ -2,6 +2,7 @@ export const MODAL_TYPE = {
   RECREATE_VOTE_MODAL: 'RECREATE_VOTE_MODAL', // 투표 재생성
   SHARE_MEETING_MODAL: 'SHARE_MEETING_MODAL', // 모임 공유
   ROOM_DETAIL_INFO_MODAL: 'ROOM_DETAIL_INFO_MODAL', // 모임 상세 정보
+  TIME_RESULT_MODAL: 'TIME_RESULT_MODAL', //날짜별 시간투표 결과
 } as const;
 
 export type ModalType = (typeof MODAL_TYPE)[keyof typeof MODAL_TYPE];
