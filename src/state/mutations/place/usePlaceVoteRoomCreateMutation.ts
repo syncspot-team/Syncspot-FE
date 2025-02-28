629ac553db9c06f526ca14ae83e8137ca0432f1b
Cllaude99 <xodbsrla1025@naver.com>
2025-02-28 16:03:47 +0900

    feat: invalidate 추가

--- src/state/mutations/place/usePlaceVoteRoomCreateMutation.ts
+++ src/state/mutations/place/usePlaceVoteRoomCreateMutation.ts
@@ -1,16 +1,26 @@
 import { postPlaceVoteRoomCreate } from '@src/apis/place/postPlaceVoteRoomCreate';
+import { ROOM_QUERY_KEY } from '@src/state/queries/header/key';
 import { IPlaceVoteRoomCreateRequestType } from '@src/types/place/placeVoteRoomCreateRequestType';
-import { useMutation, UseMutationOptions } from '@tanstack/react-query';
+import {
+  useMutation,
+  UseMutationOptions,
+  useQueryClient,
+} from '@tanstack/react-query';
 import { useParams } from 'react-router-dom';
 
 export const usePlaceVoteRoomCreateMutation = (
   options?: UseMutationOptions<any, Error, IPlaceVoteRoomCreateRequestType>,
 ) => {
   const { roomId } = useParams();
-
+  const queryClient = useQueryClient();
   return useMutation({
     mutationFn: (placeVoteRoomCreatePayload: IPlaceVoteRoomCreateRequestType) =>
       postPlaceVoteRoomCreate(roomId!, placeVoteRoomCreatePayload),
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ROOM_QUERY_KEY.GET_PLACE_VOTE_ROOM_EXISTS(roomId!),
+      });
+    },
     ...options,
   });
 };
