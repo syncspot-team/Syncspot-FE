18e884d8d0a5608c23a36d95f1323ca37b0f8695
TaeYoon Kim <xodbsrla1025@naver.com>
2025-03-02 14:08:40 +0900

    [Feature] 계정 삭제시 한번 더 묻도록 개선 (#196)
    * design: 전체 모임 목록 모바일 뷰 수정
    
    * design: 계정 탈퇴시 한번 더 묻도록 개선
    
    * design: 장소 투표 생성하기 및 투표하기 UI수정
    
    * design: 장소 투표 결과화면 모바일 UI 수정

--- src/types/modalType.ts
+++ src/types/modalType.ts
@@ -2,6 +2,7 @@ export const MODAL_TYPE = {
   RECREATE_VOTE_MODAL: 'RECREATE_VOTE_MODAL', // 투표 재생성
   SHARE_MEETING_MODAL: 'SHARE_MEETING_MODAL', // 모임 공유
   ROOM_DETAIL_INFO_MODAL: 'ROOM_DETAIL_INFO_MODAL', // 모임 상세 정보
+  QUIT_MODAL: 'QUIT_MODAL', // 계정 삭제
 } as const;
 
 export type ModalType = (typeof MODAL_TYPE)[keyof typeof MODAL_TYPE];
