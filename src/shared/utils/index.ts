b08629fc67e133b326a4a00e76d28fc6cd677f1a
“chae-dahee” <cdh010126r@gmail.com>
2025-05-20 19:17:43 +0900

    feat: fsd shared 레이어 - utils 전역 유틸리티

---
+++ src/shared/utils/index.ts
@@ -0,0 +1,7 @@
+export { default as getAPIResponseData } from './getAPIResponseData';
+export { getErrorData } from './getErrorData';
+export { mergeClassNames } from './mergeClassNames';
+export { renderShareButton } from './renderShareButton';
+export { initSentry } from './sentry';
+export { captureException, captureApiError } from './sentryCapture';
+export { sentryCreateBrowserRouter } from './sentryRouterIntegration';
