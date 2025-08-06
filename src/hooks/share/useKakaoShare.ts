3ade996dab6614e1eb8924a80b19dfac68bd9a25
채다희 <cdh010126r@gmail.com>
2025-08-06 23:07:13 +0900

    [BUG] public 이미지 소스 사용으로 변경 (#251)
    * fix: public 이미지 소스 사용으로 변경
    
    * fix: h 630 > 600 으로 2:1 비율 조정
    
    * fix: 이미지 cdn 경로 사용
    
    ---------
    
    Co-authored-by: “chae-dahee” <“cdh010126r@gmail.com”>

--- src/hooks/share/useKakaoShare.ts
+++ src/hooks/share/useKakaoShare.ts
@@ -12,8 +12,7 @@ export function useShareKakao({ descriptionType, url }: IShareKakao) {
       content: {
         title: 'Syncspot | 모두가 편하게 만나는 지름길',
         description: descriptionType,
-        imageUrl:
-          'https://private-user-images.githubusercontent.com/96279437/420756408-414c6915-f91c-4fe0-8be5-eb4f6c92523c.jpeg?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3NDQ4NzA2ODksIm5iZiI6MTc0NDg3MDM4OSwicGF0aCI6Ii85NjI3OTQzNy80MjA3NTY0MDgtNDE0YzY5MTUtZjkxYy00ZmUwLThiZTUtZWI0ZjZjOTI1MjNjLmpwZWc_WC1BbXotQWxnb3JpdGhtPUFXUzQtSE1BQy1TSEEyNTYmWC1BbXotQ3JlZGVudGlhbD1BS0lBVkNPRFlMU0E1M1BRSzRaQSUyRjIwMjUwNDE3JTJGdXMtZWFzdC0xJTJGczMlMkZhd3M0X3JlcXVlc3QmWC1BbXotRGF0ZT0yMDI1MDQxN1QwNjEzMDlaJlgtQW16LUV4cGlyZXM9MzAwJlgtQW16LVNpZ25hdHVyZT04MzUwN2UzZGRlMjYzZDBkZTU3ZTE3OTcxMTNiZmQ4OWI0NWNiNDVkMzYwYmJlYWE1ZmY3MDFmNDc4ZTU4YTFhJlgtQW16LVNpZ25lZEhlYWRlcnM9aG9zdCJ9.HUTd-yJ0ImksQ_9bbAPjEo_vzffCgLDj6VG27XIBIGo',
+        imageUrl: 'https://syncspot.kr/ogImage.jpg',
         link: {
           webUrl: url,
         },
